cd97bb5361249b720e32608d148b3d4a516877d9
Yaroslav Sivakov <yar3333@gmail.com>
2015-07-28 22:44:59 +0300

    * simplifications

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace FlaToSwf
 {
 	static class Program
 	{
-		private static readonly string WAITER_NAME = @"Global\FlaToSwfWaiter";
+		const string WAITER_NAME = @"Global\FlaToSwfWaiter";
 
 		[STAThread]
 		static int Main(string[] args)
@@ -20,44 +20,63 @@ namespace FlaToSwf
 			{
 				if (args[0] == "-complete")
 				{
-					try
-					{
-						var waiter = EventWaitHandle.OpenExisting(WAITER_NAME);
-						waiter.Set();
-					}
+					try { EventWaitHandle.OpenExisting(WAITER_NAME).Set(); }
 					catch (WaitHandleCannotBeOpenedException) {}
+				}
+				else if (args[0] == "-start")
+				{
+					runJsfl("");
+				}
+				else if (args[0] == "-quit")
+				{
+					runJsfl("fl.quit();");
+				}
+				else if (args[0].StartsWith("-"))
+				{
+					Console.WriteLine("Unknow switch '" + args[0] + "'.");
+					return 1;
 				}
 				else
 				{
-					var users = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
-					var rule = new EventWaitHandleAccessRule(users, EventWaitHandleRights.Synchronize | EventWaitHandleRights.Modify, AccessControlType.Allow);
-					var security = new EventWaitHandleSecurity();
-					security.AddAccessRule(rule);
-
-					bool created;
-					var waiter = new EventWaitHandle(false, EventResetMode.AutoReset, WAITER_NAME, out created, security);
-					var jsflFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".jsfl";
-					
-					publish(args[0], jsflFilePath);
-					
-					waiter.WaitOne();
-					try { File.Delete(jsflFilePath); } catch { }
+					runJsfl
+					(
+						"var doc = fl.openDocument('file:///" + Path.GetFullPath(args[0].Replace("/", "\\")).Replace("\\", "/") + "');\n" +
+						"doc.publish();\n" +
+						"doc.close();\n"
+					);
 				}
 				
 				return 0;
+			}
+			else
+			{
+				Console.WriteLine("Usage: FlaToSwf <pathToSourceFla> | -start | -quit");
+				Console.WriteLine("\t<pathToSourceFla> Fla document to publish. This can be also *.xfl document.");
+				Console.WriteLine("\t-start            Just run the Flash (don't need in regular).");
+				Console.WriteLine("\t-quit             Send 'quit' command to the Flash (don't need in regular).");
 			}
 
 			return 1;
 		}
 
-		static void publish(string flaPath, string tempJSFLfilepath)
+		static void runJsfl(string jsfl)
 		{
-			var fileuri = "file:///" + flaPath.Replace("\\", "/");
-			var template = Encoding.Default.GetString(FlaToSwf.Properties.Resources.TemplateJsfl)
-				.Replace("{FILE_PATH_TO_OPEN}", HttpUtility.JavaScriptStringEncode(fileuri))
-				.Replace("{COMPLETE_COMMAND}", HttpUtility.JavaScriptStringEncode("\"" + Environment.GetCommandLineArgs()[0] + "\"" + " -complete"));
-			File.WriteAllText(tempJSFLfilepath, template);
-			Process.Start(tempJSFLfilepath);
+			jsfl += "FLfile.runCommandLine('" + HttpUtility.JavaScriptStringEncode("\"" + Environment.GetCommandLineArgs()[0] + "\"" + " -complete") + "');";
+
+			var users = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+			var rule = new EventWaitHandleAccessRule(users, EventWaitHandleRights.Synchronize | EventWaitHandleRights.Modify, AccessControlType.Allow);
+			var security = new EventWaitHandleSecurity();
+			security.AddAccessRule(rule);
+
+			bool created;
+			var waiter = new EventWaitHandle(false, EventResetMode.AutoReset, WAITER_NAME, out created, security);
+			var jsflFilePath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".jsfl";
+
+			File.WriteAllText(jsflFilePath, jsfl);
+			Process.Start(jsflFilePath);
+					
+			waiter.WaitOne();
+			try { File.Delete(jsflFilePath); } catch { }
 		}
 	}
 }
--- template.jsfl
+++
@@ -1,4 +0,0 @@
-var doc = fl.openDocument("{FILE_PATH_TO_OPEN}");
-doc.publish();
-doc.close();
-FLfile.runCommandLine("{COMPLETE_COMMAND}");
